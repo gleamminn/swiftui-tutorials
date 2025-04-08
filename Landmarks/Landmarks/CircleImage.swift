399c8c823d968dc9144692090700c7fc5e5012a2
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2025-04-09 00:59:54 +0200

    1_CreatingAndCombiningViews

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 김수민 on 4/9/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김수민 on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+//@main 속성 : 앱의 진입점을 식별
+//app life cycle
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    //body 프로퍼티 : 디스플레이를 위한 콘텐츠를 제공하는 하나 이상의 scene 리턴
+}
