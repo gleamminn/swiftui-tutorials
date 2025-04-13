56e986361c575e908cb02ff8d4be834e2135e658
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2025-04-13 23:42:41 +0200

    4_DrawingPathsAndShapes

---
+++ Landmarks/Landmarks/Badges/RotateBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by 김수민 on 4/13/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
