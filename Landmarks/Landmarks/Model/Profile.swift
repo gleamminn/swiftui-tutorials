eb8207361bbb3362002fb3bffa7aa4cb0b7c92bf
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2025-04-19 23:56:22 +0200

    7_WorkingWithUIControls

---
+++ Landmarks/Landmarks/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by 김수민 on 4/19/25.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "soomin")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
